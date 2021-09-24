20fd6844b872742b34314894c66b7825fddf639c
unknown <aarushsalgania06@gmail.com>
2021-09-24 20:25:18 +0530

    Added 2 digits of precision

--- Beginner/ATM.js
+++ Beginner/ATM.js
@@ -10,9 +10,10 @@ const readline = require('readline').createInterface({
 
 readline.question("Please enter the amount you want to withdraw(Must be multiple of 5) And your total balance(Split with a space)", input => {
     let wda = parseInt(input.split(" ")[0])
-    let bal = parseInt(input.split(" ")[1])
+    let bal = parseFloat(input.split(" ")[1]).toFixed(2)
     if (isNaN(wda) || isNaN(bal)) return console.log("Not a number")
+    if (bal <= 0 || bal >= 2000 || wda <= 0 || wda >= 2000) return console.log("Wrong Input")
     if (wda % 5 !== 0) return console.log(bal)
-    if (bal < wda) return console.log(wda)
-    console.log(bal - wda - 0.50)
+    if (bal < wda) return console.log(bal)
+    console.log((bal - wda - 0.50).toFixed(2))
 })
